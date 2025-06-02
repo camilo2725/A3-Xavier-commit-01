7c1ed5a29b1b2d73b4bec4fb4aabdc28755e8613
Stauncho <breninhodantinhas@outlook.com>
2025-06-02 21:52:22 +0000

    Update gerente.js

--- backend/src/routes/gerente.js
+++ backend/src/routes/gerente.js
@@ -6,19 +6,13 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
-      ? {
-          connectionString: process.env.DATABASE_URL,
-          ssl: { rejectUnauthorized: false }
-        }
-      : {
-          host: process.env.KNEX_HOST,
-          user: process.env.KNEX_USER,
-          password: process.env.KNEX_PASS,
-          database: process.env.KNEX_DATA,
-          port: 5432,
-          ssl: { rejectUnauthorized: false }
-        },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   },
 };
 
