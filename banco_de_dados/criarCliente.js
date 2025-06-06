4850d6bf064f97e5d10fe867588edbb77f277a20
amaralarthur2 <amaralarthur155@gmail.com>
2025-06-06 17:28:58 -0300

    removi o supabase

--- banco_de_dados/criarCliente.js
+++
@@ -1,5 +0,0 @@
-import { creatClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://euewpnutlftkbezmvhjl.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZXdwbnV0bGZ0a2Jlem12aGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NTQ4MzYsImV4cCI6MjA2MzQzMDgzNn0.gUr-BFbXcXuwrnq-X3U0B7QaWeMamZ48u3mdtRiYwAs'
-const supabase = CreatClient(supabaseUrl, supabaseKey)
