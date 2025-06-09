e6c288420c196445efeb6dd4a9c14780439c8627
camilo2725 <camilofondado2705@gmail.com>
2025-06-08 22:49:55 -0300

    Concertando scroll tabela (index.js)

--- frontend/src/components/Tabela/index.js
+++ frontend/src/components/Tabela/index.js
@@ -3,26 +3,27 @@ import './Tabela.css';
 
 export const Tabela = ({ colunas, dados }) => {
     return (
-
-        <div class="table-responsive">
-            <table className="table table-striped table-dark tabela-personalizada">
-                <thead>
-                    <tr>
-                        {colunas.map((coluna, index) => (
-                            <th key={index}>{coluna}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {dados.map((linha, idx) => (
-                        <tr key={idx}>
-                            {colunas.map((coluna, i) => (
-                                <td key={i}>{linha[coluna]}</td>
+        <div className="tabela-container">
+            <div className="table-wrapper">
+                <table className="table table-striped table-dark tabela-personalizada">
+                    <thead>
+                        <tr>
+                            {colunas.map((coluna, index) => (
+                                <th key={index}>{coluna}</th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div >
+                    </thead>
+                    <tbody>
+                        {dados.map((linha, idx) => (
+                            <tr key={idx}>
+                                {colunas.map((coluna, i) => (
+                                    <td key={i}>{linha[coluna]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 };
